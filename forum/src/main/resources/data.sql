7b4051be1bee352ca66940dcac49b2f7d7778c87
HarturAlcantaraDev <112022106+HarturAlcantaraDev@users.noreply.github.com>
2022-11-21 14:37:23 -0300

    implementando camada de segurança na nossa aplicação, criando classe SecurityConfigurations e AutenticacaoService. Usando o BCryptPassawordEnconder para não deixarmos a senha exposta no banco de dados e sim o seu hash

--- forum/src/main/resources/data.sql
+++ forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$l6mX7I0xekM39sOtzH5y9OWsFAel0MIV4k1Mq1EPEHSynt8YupQka');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
